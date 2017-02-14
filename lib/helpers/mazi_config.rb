2c1130df682f7136891a9719c154d3cb67eb66be
ardadouk <ardadouk@gmail.com>
2017-02-14 13:33:41 +0200

    comments on top of config file

--- lib/helpers/mazi_config.rb
+++ lib/helpers/mazi_config.rb
@@ -1,5 +1,28 @@
 require 'yaml'
 
+TOPCOMMENTS = <<TEXT
+# This is the main configuration file for the mazizone portal.
+# If you manually change some values on this file a server restart is
+# mandatory for the changes to take effect.
+# On the admin section:
+# - admin_username: admin username for the admin panel
+# - admin_password: admin password for the admin panel.
+# On the scripts section:
+# - backend_scripts_folder: the folder that contains the scripts that the portal 
+#                           will need to execute for various actions
+# - enabled_scripts: the scripts that are allowed to be executed by the 
+#                    backend (for security reasons)
+# On the portal configuration section:
+# - title: the title on both the top left corner and on browser title
+# - applications_title: title on the applications body page
+# - applications_subtitle: subtitle on the applications body page
+# - applications_welcome_text: welcome text on the applications body page
+# - side_panel_color: side panel main color (hex value)
+# - side_panel_active_color: side panel active and on hover color (hex value)
+# - top_panel_color: top panel main color (hex value)
+# - side_panel_active_color: top panel active and on hover color (hex value)
+TEXT
+
 module MaziConfig
   # loads the configuration file
   def loadConfigFile(file='etc/config.yml')
@@ -31,6 +54,16 @@ module MaziConfig
   def writeConfigFile(conf=nil, file='etc/config.yml')
     @config = conf.nil? ? readConfigFile : conf
     File.open(file, 'w+') { |f| YAML.dump(@config, f) }
+    f = File.open(file, "r+")
+      lines = f.readlines
+    f.close
+
+    str = TOPCOMMENTS
+    lines.each  { |line| str += line }
+
+    output = File.new(file, "w")
+      output.write str
+    output.close
     @config
   end
 end
