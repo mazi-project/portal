d6cca7df2091a4058f8047271acded997215e188
ardadouk <ardadouk@gmail.com>
2017-02-14 13:35:59 +0200

    alert dialog on admin application delete button

--- public/js/admin_application.js
+++ public/js/admin_application.js
@@ -1,17 +1,36 @@
 $( document ).ready(function() {
   console.log( "ready!" );
+  var confirm_element_id = null;
+
+  $( "#delete-dialog-confirm" ).dialog({
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    autoOpen: false,
+    buttons: {
+      Yes: function() {
+        $.ajax({
+          url: '/application/' + confirm_element_id,
+          type: 'DELETE',
+          success: function(result) {
+            id = JSON.parse(result).id;
+            $('#app_tr_' + id).remove();
+          }
+        });
+        $( this ).dialog( "close" );
+      },
+      No: function() {
+        confirm_element_id = null;
+        $( this ).dialog( "close" );
+      }
+    }
+  });
 
   $('.del_app').click(function(){
-    var appId = $(this).attr('id').split('_').pop();
-    console.log(appId);
-    $.ajax({
-      url: '/application/' + appId,
-      type: 'DELETE',
-      success: function(result) {
-        id = JSON.parse(result).id;
-        $('#app_tr_' + id).remove();
-      }
-    });
+    confirm_element_id = $(this).attr('id').split('_').pop();
+    console.log(confirm_element_id);
+    $( "#delete-dialog-confirm" ).dialog( "open" );
   });
 
   $('.enabled-switch').click(function(){
