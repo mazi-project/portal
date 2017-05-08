cd2196272d798bd14379b6520d00076377191576
ardadouk <ardadouk@gmail.com>
2017-05-08 12:56:14 +0300

    testing update

--- mazi_portal_server.rb
+++ mazi_portal_server.rb
@@ -916,7 +916,7 @@ class MaziApp < Sinatra::Base
 
     Thread.new do
       sleep 2
-      MaziLogger.debug 'Restarting Now'
+      MaziLogger.debug 'Restarting'
       `service mazi-portal restart`
     end
 
