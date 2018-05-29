6f3481d8d80d0c5c73542633ae5d30e9823152fe
Aris Dadoukis <ardadouk@gmail.com>
2018-05-29 15:55:40 +0300

    typo fix

--- database/migrations/008_add_application_color_string.rb
+++ database/migrations/008_add_application_color_string.rb
@@ -1,11 +1,11 @@
 Sequel.migration do
   up do
-    add_column :applications, :type, String
-    add_column :application_instances, :type, String
+    add_column :applications, :color, String
+    add_column :application_instances, :color, String
   end
 
   down do
-    drop_column :applications, :type
-    drop_column :application_instances, :type
+    drop_column :applications, :color
+    drop_column :application_instances, :color
   end
 end
